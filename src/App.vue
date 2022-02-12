b7b98017510efb2ea36ac1fc65880d8d407e88be
kakahikari <kakahikari@gmail.com>
2022-02-13 00:08:35 +0800

    add sass-loader

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template lang="pug">
 .app
 </template>
-
 <script>
 
 export default {
   name: 'App',
 }
 </script>
+<style lang="scss">
+$apple: red;
 
-<style>
-#app {
-  margin-top: 60px;
+.app {
+  min-width: 100vw;
+  min-height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: blue;
 }
 </style>
